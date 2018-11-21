43ea364205ab0b4b2853c6de6ac421eafe0e48b4
Lukasz Czechowski <ext-lukasz.czechowski@finago.com>
2018-11-21 10:05:47 +0100

    allure

--- src/test/java/Steps/SmokeTests.java
+++ src/test/java/Steps/SmokeTests.java
@@ -1,15 +1,26 @@
 package Steps;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.Test;
+
+@Epic("To jest epic")
+@Feature("To jest feature")
 
 public class SmokeTests {
 
     @Test (groups = {"smoke","regression"})
+    @Story("This is smoke test")
+    @Description("Opis testu")
     public void fakeSmokeTest(){
 
     }
 
     @Test (groups = ("regression"))
+    @Story("This is smoke test")
+    @Description("Opis testu")
     public void fakeRegressionTest(){
 
     }
